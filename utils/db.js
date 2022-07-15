a7c5a804fd86fb2e36e0748ffd7c2eb9503ff18c
loveisgala <gabriela.gk@hotmail.com>
2022-07-15 16:23:08 -0300

    create endpoint

--- utils/db.js
+++ utils/db.js
@@ -4,8 +4,10 @@ const MongoClient = mongodb.MongoClient;
 
 async function pool() {
   try {
-    return (await MongoClient.connect("mongodb://localhost:27017"));
+    return (await MongoClient.connect("mongodb://localhost:27017")).db("partidos");
   } catch (e) {
     console.error(e.stack);
   }
 }
+
+module.exports = { pool };
